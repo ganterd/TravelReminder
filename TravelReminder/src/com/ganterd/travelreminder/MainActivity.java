1ac800bde744125e54ae08f2798ca7d170d8e08b
unknown <I072979@DUBN60266102A.emea.global.corp.sap>
2014-05-13 14:46:57 +0100

    Adding Google Play Services

--- TravelReminder/src/com/ganterd/travelreminder/MainActivity.java
+++ TravelReminder/src/com/ganterd/travelreminder/MainActivity.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 
 public class MainActivity extends ListActivity {
 
