491b19206b3f7070984b2522fb379e7650616bb4
ganterd <ganterd@tcd.ie>
2014-05-20 17:24:10 +0100

    Get directions and draw path

---
+++ TravelReminder/src/com/ganterd/travelreminder/Directions.java
@@ -0,0 +1,151 @@
+package com.ganterd.travelreminder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class Directions {
+	protected LatLng origin;
+	protected LatLng destination;
+	protected OnDirectionsReadyListener listener;
+	protected String mode = "driving";
+	
+	public interface OnDirectionsReadyListener{
+		public void onDirectionsReady(JSONObject directions);
+	}
+	
+	public void setOrigin(LatLng origin){
+		this.origin = origin;
+	}
+	
+	public void setDestination(LatLng destination){
+		this.destination = destination;
+	}
+	
+	public void setMode(String mode){
+		this.mode = mode;
+	}
+	
+	public void requestDirections(OnDirectionsReadyListener listener){
+		this.listener = listener;
+		new Requestor().execute();
+	}
+        
+	public static PolylineOptions getDirectionsPath(JSONObject directions){
+		PolylineOptions polyLine = new PolylineOptions();
+		
+		try {
+			
+			
+			JSONArray routes = directions.getJSONArray("routes");
+			
+			if(routes.length() > 0){
+				JSONObject route = (JSONObject) routes.get(0);
+				if(route.has("overview_polyline")){
+					JSONObject overview_polyline = (JSONObject)route.get("overview_polyline");
+					
+					ArrayList<LatLng> points = decodePoly(overview_polyline.getString("points"));
+					for(LatLng p : points){
+						polyLine.add(p);
+					}
+				}
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		return polyLine;
+	}
+	
+	private class Requestor extends AsyncTask<Void, Void, JSONObject>{
+		@Override
+		protected JSONObject doInBackground(Void... params){
+			String url = "http://maps.googleapis.com/maps/api/directions/json?";
+			url += "origin=" + origin.latitude + "," + origin.longitude;
+			url += "&destination=" + destination.latitude + "," + destination.longitude;
+			url += "&sensor=false";
+			url += "&mode=" + mode;
+			
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpContext localContext = new BasicHttpContext();
+			HttpGet httpGet = new HttpGet(url);
+			
+			try {
+				
+				HttpResponse resp = httpClient.execute(httpGet, localContext);
+				
+				if(resp.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+					String data = EntityUtils.toString(resp.getEntity());
+					JSONObject obj;
+					try {
+						obj = new JSONObject(data);
+						return obj;
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+					
+				}
+				
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			return new JSONObject();
+		}
+		
+		@Override
+		protected void onPostExecute(JSONObject directions){
+			listener.onDirectionsReady(directions);
+		}
+	}
+	
+	private static ArrayList<LatLng> decodePoly(String encoded) {
+		ArrayList<LatLng> points = new ArrayList<LatLng>();
+		
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;                 
+                shift += 5;             
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;  
+                shift += 5;             
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+ 
+            LatLng position = new LatLng((double) lat / 1E5, (double) lng / 1E5);
+            points.add(position);
+        }
+        
+        return points;
+	}
+}
--- TravelReminder/src/com/ganterd/travelreminder/fragments/ReminderEditLocationInfo.java
+++ TravelReminder/src/com/ganterd/travelreminder/fragments/ReminderEditLocationInfo.java
@@ -1,14 +1,19 @@
 package com.ganterd.travelreminder.fragments;
 
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
+import com.ganterd.travelreminder.Directions;
+import com.ganterd.travelreminder.Directions.OnDirectionsReadyListener;
 import com.ganterd.travelreminder.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
@@ -18,14 +23,19 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class ReminderEditLocationInfo extends Fragment {
+public class ReminderEditLocationInfo extends Fragment implements OnDirectionsReadyListener{
 	private SupportMapFragment mapFragment;
 	private GoogleMap map;
 	private GoogleMapOptions mapOptions = new GoogleMapOptions();
 	
 	private LatLng origin = null;
 	private LatLng destination = null;
+	
+	private PolylineOptions path = null;
+	
+	private Directions directions = new Directions();
 	
 	private GoogleMap.OnMapClickListener clickListener = new GoogleMap.OnMapClickListener() {
 		@Override
@@ -40,6 +50,13 @@ public class ReminderEditLocationInfo extends Fragment {
 		mapOptions.camera(new CameraPosition(new LatLng(53.3282161, -6.2348722), 11, 0, 0));
 		
 		RelativeLayout rl = (RelativeLayout)inflater.inflate(R.layout.reminder_details_location_info, container, false);
+		
+		EditText startPointInput = (EditText) rl.findViewById(R.id.textReminderStartPoint);
+		startPointInput.setInputType(EditorInfo.TYPE_NULL);
+		
+		EditText endPointInput = (EditText) rl.findViewById(R.id.textReminderEndPoint);
+		endPointInput.setInputType(EditorInfo.TYPE_NULL);
+		
 		return rl;
 	}
 	
@@ -82,11 +99,35 @@ public class ReminderEditLocationInfo extends Fragment {
 			map.addMarker(new MarkerOptions().position(destination).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
 	}
 	
+	public void drawPath(){
+		if(path != null){
+			map.addPolyline(path);
+		}
+	}
+	
+	@Override
+	public void onDirectionsReady(JSONObject directions){
+		this.path = Directions.getDirectionsPath(directions);
+		drawPath();
+	}
+	
 	public void setPosition(LatLng p){
-		if(((EditText)getView().findViewById(R.id.textReminderStartPoint)).hasFocus()){
+		EditText originInput = (EditText)getView().findViewById(R.id.textReminderStartPoint);
+		EditText destinationInput = (EditText)getView().findViewById(R.id.textReminderEndPoint);
+		
+		if(originInput.hasFocus()){
 			origin = p;
-		}else if(((EditText)getView().findViewById(R.id.textReminderEndPoint)).hasFocus()){
+			originInput.setText(origin.toString());
+		}else if(destinationInput.hasFocus()){
 			destination = p;
+			destinationInput.setText(destination.toString());
+		}
+		
+		if(origin != null && destination != null){
+			directions.setOrigin(origin);
+			directions.setDestination(destination);
+			directions.setMode("driving");
+			directions.requestDirections(this);
 		}
 		
 		drawMarkers();
